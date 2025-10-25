ab9190e1334af88171eb9033a3d9c9883e56dfd4
huok <77536370+dee-shar@users.noreply.github.com>
2025-10-26 01:53:44 +0300

    Update MinecraftRating.cs

--- src/MinecraftRating.cs
+++ src/MinecraftRating.cs
@@ -1,34 +1,40 @@
 using System;
 using System.Text;
 using System.Net.Http;
-using System.Threading.Tasks;
 
-namespace MinecraftRating {
-    public class MinecraftRatingApi {
+namespace MinecraftRating
+{
+    public class MinecraftRatingApi
+    {
         private readonly HttpClient httpClient;
         private readonly string apiUrl = "https://minecraftrating.ru/api";
-        public MinecraftRatingApi() {
+        public MinecraftRatingApi()
+        {
             httpClient = new HttpClient();
             httpClient.DefaultRequestHeaders.Add("User-Agent",
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36");
         }
 
-        public async Task<string> RegisterAccount(string login, string email, string password) {
+        public async Task<string> RegisterAccount(string login, string email, string password)
+        {
             var data = $"login={login}&email={email}&password={password}";
             return await PostAsync("/register/", data);
         }
 
-        public async Task<string> Login(string username, string password) {
+        public async Task<string> Login(string username, string password)
+        {
             var data = $"login_name={username}&login_password={password}&login=submit";
             return await PostAsync("/register/", data);
         }
 
-        public async Task<string> AddServer(string serverAddress) {
+        public async Task<string> AddServer(string serverAddress)
+        {
             var data = $"ip={serverAddress}";
             return await PostAsync("/add-server/", data);
         }
 
-        private async Task<string> PostAsync(string endpoint, string formData) {
+        private async Task<string> PostAsync(string endpoint, string formData)
+        {
             var content = new StringContent(formData, Encoding.UTF8, "application/x-www-form-urlencoded");
             var response = await httpClient.PostAsync($"{apiUrl}{endpoint}", content);
             return await response.Content.ReadAsStringAsync();
